633c4c13db2bca690f438ad20e1c9f1bded2bf3b
JdaraD <155355385+JdaraD@users.noreply.github.com>
2024-05-07 08:50:17 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller {
+    public function index()
+    {
+        echo "<h1>perkenalan</h1>";
+        echo"Nama Saya Julian Diwandara
+             saya tingga di parung
+             olah raga yang saya sukai dalah sepaktakraw";
+    }
+}
